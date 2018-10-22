516a514e1400c42b282d56e7ed5266363c4ecbb5
franco@mumuki.org <franco@mumuki.org>
2018-10-22 11:24:42 -0400

    Mumuki Export on 2018-10-22 11:24:42 -0400

--- 00001_Declarando a Juan Perez/test.java
+++ 00001_Declarando a Juan Perez/test.java
@@ -1,1 +1,8 @@
-[{"name":"Juan perez debe tener 25","keys":{"first_name":{"equal":"Juan"},"age":{"equal":"25"},"last_name":{"equal":"Perez"}}}]
+- name: Juan perez debe tener 25
+  keys:
+    first_name: 
+      equal: Juan
+    age:
+      equal: 25
+    last_name:
+      equal: Perez
