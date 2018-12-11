2dcf446b4b2242982f1099b1ebfd126f9413c889
gus.trucco@gmail.com <gus.trucco@gmail.com>
2018-12-11 12:50:18 -0500

    Mumuki Export on 2018-12-11 12:50:18 -0500

--- 00002_Obtención de IP/test.java
+++ 00002_Obtención de IP/test.java
@@ -1,5 +1,4 @@
-- name: La dirección ip tiene el formato correcto
+- name: La ip tiene el formato correcto
   keys:
     ip_address: 
       match: !ruby/regexp '/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i'
-    
