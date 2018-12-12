25d65214cfbad1911f97b24e039b1e8af75e2f2e
gus.trucco@gmail.com <gus.trucco@gmail.com>
2018-12-12 15:14:42 -0500

    Mumuki Export on 2018-12-12 15:14:42 -0500

--- 00004_Ejercicio PING/test.java
+++ 00004_Ejercicio PING/test.java
@@ -1,4 +1,5 @@
 - name: El tiempo de ping tiene el formato correcto
-  ping_time:
-    match: !ruby/regexp '/^[0-9]{1,3}\.[0-9]{1,3}\s{0,1}\ms$/i'
+  keys:
+    ping_time:
+      match: !ruby/regexp '/^[0-9]{1,3}\.[0-9]{1,3}\s{0,1}\ms$/i'
     
