06cf26c26b8d2865a090a455bec8dc916567636f
gus.trucco@gmail.com <gus.trucco@gmail.com>
2019-01-09 14:18:48 -0500

    Mumuki Export on 2019-01-09 14:18:48 -0500

---
+++ 00005_Ejercicio GET/description.md
@@ -0,0 +1,1 @@
+Prueba de GET para obtener feriados
---
+++ 00005_Ejercicio GET/test.java
@@ -0,0 +1,4 @@
+- name: El motivo del segundo feriado es correcto
+  keys:
+    ip_address: 
+      match: !ruby/regexp '/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i'
